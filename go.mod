d4e434fc6070630c8f1c607fb9fdd1d21a40a953
Zachareee <zacharytanhx@yahoo.com>
2026-01-14 21:20:45 +0800

    feat: upload function, info, validate, extract_credentials function working

--- go.mod
+++ go.mod
@@ -3,6 +3,11 @@ module github.com/Zachareee/savesync_gdrive
 go 1.25.5
 
 require (
+	golang.org/x/oauth2 v0.33.0
+	google.golang.org/api v0.257.0
+)
+
+require (
 	cloud.google.com/go/auth v0.17.0 // indirect
 	cloud.google.com/go/auth/oauth2adapt v0.2.8 // indirect
 	cloud.google.com/go/compute/metadata v0.9.0 // indirect
@@ -20,10 +25,8 @@ require (
 	go.opentelemetry.io/otel/trace v1.38.0 // indirect
 	golang.org/x/crypto v0.45.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/oauth2 v0.33.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
-	google.golang.org/api v0.257.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20251124214823-79d6a2a48846 // indirect
 	google.golang.org/grpc v1.77.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
--- main.go
+++ main.go
@@ -1,83 +1,235 @@
 package main
 
+/*
+#include <stdlib.h>
+
+struct Info {
+	char *name;
+	char *description;
+	char *author;
+	char *icon_url;
+};
+
+struct FileDetails {
+	char *tag;
+	char *filename;
+	unsigned long long timestamp;
+};
+*/
+import "C"
+
 import (
+	"bytes"
 	"context"
+	"crypto/rand"
+	"encoding/json"
+	"net/url"
+	"time"
+	"unsafe"
+
 	"fmt"
 	"log"
+
 	"net/http"
-	// "strings"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	"google.golang.org/api/drive/v3"
-	"google.golang.org/api/googleapi"
 	"google.golang.org/api/option"
+	// "google.golang.org/api/googleapi"
+	// "google.golang.org/api/option"
 )
 
-func getClient(config *oauth2.Config) *http.Client {
-	tok := getTokenFromWeb(config)
+type CStr = *C.char
 
-	return config.Client(context.Background(), tok)
-}
+var ctx = context.Background()
 
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+const data = `{"installed":{"client_id":"487698375903-j8s33ij1pc335jc2pu6d2rb1bgrg2fqo.apps.googleusercontent.com","project_id":"savesync-450104","auth_uri":"https://accounts.google.com/o/oauth2/auth","token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"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","redirect_uris":["http://localhost"]}}`
 
-	fmt.Printf("%v", authURL)
-
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
-	}
-
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
-	}
-
-	fmt.Println(tok)
-	return tok
-}
-
-var data = []byte("{\"installed\":{\"client_id\":\"487698375903-j8s33ij1pc335jc2pu6d2rb1bgrg2fqo.apps.googleusercontent.com\",\"project_id\":\"savesync-450104\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://oauth2.googleapis.com/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_secret\":\"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\",\"redirect_uris\":[\"http://localhost\"]}}")
-
-func main() {
-	ctx := context.Background()
-
-	config, err := google.ConfigFromJSON(data, drive.DriveFileScope)
+func getConfig() *oauth2.Config {
+	config, err := google.ConfigFromJSON([]byte(data), drive.DriveFileScope)
 
 	if err != nil {
 		log.Fatalf("Unable to parse client secret file to config: %v", err)
 	}
-	client := getClient(config)
+	return config
+}
 
-	srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
-
-	if err != nil {
-		log.Fatalf("Unable to retrieve Drive client: %v", err)
-	}
-
-	f, err := srv.Files.Create(&drive.File{Name: "SaveSync", MimeType: "application/vnd.google-apps.folder"}).Do()
-	//.Media(strings.NewReader("Hello"))
+func getClient(config *oauth2.Config, data []byte) *http.Client {
+	token, err := getToken(data)
 
 	if err != nil {
-		log.Fatalf("Unable to create folder: %v", err)
+		log.Fatalf("Unable to parse token: %v", err)
 	}
 
-	fmt.Println(f.Id)
+	return config.Client(ctx, &token)
+}
 
-	r, err := srv.Files.List().PageSize(10).Fields(googleapi.Field("files(id, name)")).Do()
+func getToken(data []byte) (oauth2.Token, error) {
+	var authtoken oauth2.Token
+	err := json.Unmarshal(data, &authtoken)
 
-	if err != nil {
-		log.Fatalf("Unable to retrieve files: %v", err)
-	}
+	return authtoken, err
+}
 
-	fmt.Println("Files:")
-	if len(r.Files) == 0 {
-		fmt.Println("No files found.")
-	} else {
-		for _, i := range r.Files {
-			fmt.Printf("%s (%s)\n", i.Name, i.Id)
-		}
+func createAuthCodeURL(redirect_uri string) string {
+	config := getConfig()
+	config.RedirectURL = redirect_uri
+	return config.AuthCodeURL(rand.Text(), oauth2.AccessTypeOffline)
+}
+
+//export info
+func info() C.struct_Info {
+	return C.struct_Info{
+		name:        C.CString("Google Drive"),
+		description: C.CString("Google Drive plugin for SaveSync"),
+		author:      C.CString("Zachareee"),
+		icon_url:    C.CString("https://upload.wikimedia.org/wikipedia/commons/1/12/Google_Drive_icon_%282020%29.svg"),
 	}
 }
+
+//export validate
+func validate(credentials, redirect_uri CStr) (CStr, CStr) {
+	url := C.CString(createAuthCodeURL(C.GoString(redirect_uri)))
+	{
+		credentials := C.GoString(credentials)
+		if credentials == "" {
+			return url, C.CString("No credentials provided")
+		}
+
+		token, err := getToken([]byte(credentials))
+		switch {
+		case err != nil:
+			return url, C.CString(err.Error())
+		case !token.Valid():
+			return url, C.CString("Token is invalid, please reauthenticate")
+		}
+	}
+
+	free_memory(url)
+	return nil, nil
+}
+
+//export extract_credentials
+func extract_credentials(uri CStr) (CStr, CStr) {
+	s, err := url.Parse(C.GoString(uri))
+	if err != nil {
+		return nil, C.CString(fmt.Sprintf("Could not parse url: %+v", err))
+	}
+
+	queries := s.Query()
+
+	if errcode := queries.Get("error"); errcode != "" {
+		return nil, C.CString(fmt.Sprintf("Error while exchanging authorization code: %+v", errcode))
+	}
+
+	auth, err := getConfig().Exchange(context.TODO(), queries.Get("code"))
+	if err != nil {
+		return nil, C.CString(fmt.Sprintf("Unable to retrieve token from web %v", err))
+	}
+
+	data, err := json.Marshal(auth)
+
+	if err != nil {
+		fmt.Printf("Unable to convert token to json %v\n", err)
+	}
+
+	return C.CString(string(data)), nil
+}
+
+func getFileService(access_token []byte) (*drive.FilesService, error) {
+	srv, err := drive.NewService(ctx, option.WithHTTPClient(getClient(getConfig(), access_token)))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return srv.Files, nil
+}
+
+//export read_cloud
+func read_cloud() (uint64, *C.struct_FileDetails) {
+	return 0, nil
+}
+
+//export upload
+func upload(access_token, tag, filename CStr, date_modified uint64, data CStr, data_length uint64) CStr {
+	files, err := getFileService([]byte(C.GoString(access_token)))
+
+	if err != nil {
+		return C.CString(err.Error())
+	}
+
+	tagstring := C.GoString(tag)
+	savesync_folder, err := files.Create(&drive.File{Name: "SaveSync", MimeType: "application/vnd.google-apps.folder"}).Context(ctx).Do()
+
+	if err != nil {
+		return C.CString(err.Error())
+	}
+
+	folder, err := files.Create(&drive.File{Name: tagstring, MimeType: "application/vnd.google-apps.folder", Parents: []string{savesync_folder.Id}}).Context(ctx).Do()
+
+	if err != nil {
+		return C.CString(err.Error())
+	}
+
+	byteslice := unsafe.Slice((*byte)(unsafe.Pointer(data)), data_length)
+
+	_, err = files.
+		Create(&drive.File{
+			Name:         C.GoString(filename),
+			ModifiedTime: time.Unix(int64(date_modified), 0).Format(time.RFC3339),
+			Parents:      []string{folder.Id},
+		}).
+		Context(ctx).
+		Media(bytes.NewReader(byteslice)).
+		Do()
+
+	if err != nil {
+		return C.CString(err.Error())
+	}
+	return nil
+}
+
+//export download
+func download(tag, filename CStr) {}
+
+//export free_memory
+func free_memory(str *C.char) {
+	C.free(unsafe.Pointer(str))
+}
+
+//	func main() {
+//		client := getClient(getConfig())
+//
+//		srv, err := drive.NewService(ctx, option.WithHTTPClient(client))
+//
+//		if err != nil {
+//			log.Fatalf("Unable to retrieve Drive client: %v", err)
+//		}
+//
+//		f, err := srv.Files.Create(&drive.File{Name: "SaveSync", MimeType: "application/vnd.google-apps.folder"}).Do()
+//		//.Media(strings.NewReader("Hello"))
+//
+//		if err != nil {
+//			log.Fatalf("Unable to create folder: %v", err)
+//		}
+//
+//		fmt.Println(f.Id)
+//
+//		r, err := srv.Files.List().PageSize(10).Fields(googleapi.Field("files(id, name)")).Do()
+//
+//		if err != nil {
+//			log.Fatalf("Unable to retrieve files: %v", err)
+//		}
+//
+//		fmt.Println("Files:")
+//		if len(r.Files) == 0 {
+//			fmt.Println("No files found.")
+//		} else {
+//			for _, i := range r.Files {
+//				fmt.Printf("%s (%s)\n", i.Name, i.Id)
+//			}
+//		}
+//	}
+func main() {}
